17a964a700127adf32490a9e8ff7e0101637bc06
popoway <31576265+popoway@users.noreply.github.com>
2018-11-11 05:25:42 -0500

    Heroku express support
    do NOT deploy this commit; it's not done yet

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var PORT = process.env.PORT || 3000;
+
+express()
+  .use(express.static('dist'))
+  .listen(PORT, onListen);
+
+function onListen() {
+  console.log('Listening on', PORT);
+}
